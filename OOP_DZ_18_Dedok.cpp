a3d52ccb7202366735c69a1a734bd93cfa286b63
DXdok_zp32@student.itstep.org <DXdok_zp32@student.itstep.org>
2024-02-18 13:38:13 +0200

    Добавьте файлы проекта.

---
+++ OOP_DZ_18_Dedok.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+string compressText(const string& text) {
+	string compressedText;
+	bool spaceFound = false; 
+	for (char c : text) {
+		if (c == ' ') {
+			if (!spaceFound) {
+				compressedText += c;
+				spaceFound = true;
+			}
+		}
+		else {
+			compressedText += c;
+			spaceFound = false;
+		}
+	}
+	if (!compressedText.empty() && compressedText.back() == ' ') {
+		compressedText.pop_back();
+	}
+	return compressedText;
+}
+int main() {
+	string text = "Example    text     for     compressing";
+	string compressedText = compressText(text);
+	cout << "Main text: " << text << endl;
+	cout << "Compressed text: " << compressedText << endl;
+	return 0;
+}
